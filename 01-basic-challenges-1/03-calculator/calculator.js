58992204c11cdb758246be5c7b1202a7a63b66b5
Julia Darwish <darwishjulia4@gmail.com>
2024-03-19 15:49:27 -0400

    Added function Calculator

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,15 @@
-function calculator() {}
+function calculator(a, b, operator) {
+    if(operator === "+") {
+        return a + b;
+    }else if(operator === "-") {
+        return a - b;
+    } else if(operator === "/") {
+        return a / b;
+    } else if(operator === "*") {
+        return a * b;
+    }else {
+       return  new Error("Invalid operator")
+    }
+}
 
 module.exports = calculator;
