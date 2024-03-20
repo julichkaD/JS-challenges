a2035f220fd63aae32eabb7641cf2c19e8b05148
Julia Darwish <darwishjulia4@gmail.com>
2024-03-20 09:54:43 -0400

    Updated palindrome function

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,15 @@
-function isPalindrome() {}
+function isPalindrome(word) {
+  let formattedWord = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+  let reversedWord = formattedWord.split("").reverse().join("");
+
+  for (let i = 0; i <= word.length; i++) {
+    if (reversedWord === formattedWord) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
 
 module.exports = isPalindrome;
