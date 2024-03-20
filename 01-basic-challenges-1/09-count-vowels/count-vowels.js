a964816cfad79e76536a09fe047d5904ccee9280
Julia Darwish <darwishjulia4@gmail.com>
2024-03-20 11:03:59 -0400

    Updated vowels function

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,20 @@
-function countVowels() {}
+function countVowels(str) {
+  let formattedStr = str.toLowerCase();
+  let count = 0;
+
+  for (let i = 0; i < formattedStr.length; i++) {
+    const char = formattedStr[i];
+    if (
+      char === "a" ||
+      char === "o" ||
+      char === "e" ||
+      char === "i" ||
+      char === "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = countVowels;
