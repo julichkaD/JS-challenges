50bdc2d20530f74201fdda7c9561d0ff1f9c86a1
Julia Darwish <darwishjulia4@gmail.com>
2024-03-23 12:45:11 -0400

    Update nonRepeating func

---
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -0,0 +1,16 @@
+function findFirstNonRepeatingCharacter(str) {
+  let charCount = {};
+
+  for (const char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+    console.log(charCount);
+  }
+  for (const char of str) {
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+  return null;
+}
+
+module.exports = findFirstNonRepeatingCharacter;
