8846f0afe8b0d62d929829fd3c6bc04f56b8a8e0
Julia Darwish <darwishjulia4@gmail.com>
2024-03-23 10:50:21 -0400

    Update UniqueSet func

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,11 +1,10 @@
 function areAllCharactersUnique(arr) {
-   let uniqueSet = new Set(arr);
-
-   if(uniqueSet.size === arr.length) {
-    return true;
-   }else{
+  let uniqueSet = new Set(arr);
+  if (arr.length !== uniqueSet.size) {
     return false;
-   }
+  } else {
+    return true;
+  }
 }
 
 module.exports = areAllCharactersUnique;
