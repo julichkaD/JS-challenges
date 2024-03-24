c8db3466e9f9404208999430a3614758f9d99f06
Julia Darwish <darwishjulia4@gmail.com>
2024-03-23 22:53:32 -0400

    Update validateEmail func

---
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -0,0 +1,10 @@
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (emailRegex.test(email)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+module.exports = validateEmail;
