2ba1cf03e2f959720e2a3a7cbdd4aecbd5f7ad92
Julia Darwish <darwishjulia4@gmail.com>
2024-03-23 22:59:49 -0400

    Add additional func validateEmail

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,10 +1,31 @@
+// function validateEmail(email) {
+//   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//   if (emailRegex.test(email)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 function validateEmail(email) {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (emailRegex.test(email)) {
-    return true;
-  } else {
+  // Check if the email contains the "@" symbol
+  if (email.indexOf("@") === -1) {
     return false;
   }
-}
 
+  // Split the email into the local part and domain
+  const [localPart, domain] = email.split("@");
+
+  // Check if the local part and domain meet the minimum length requirements
+  if (localPart.length === 0 || domain.length < 3) {
+    return false;
+  }
+
+  // Check if the domain extension consists of at least two characters
+  const domainExtension = domain.split(".");
+  if (domainExtension.length < 2 || domainExtension[1].length < 2) {
+    return false;
+  }
+ // If all checks pass, return true
+  return true;
+}
 module.exports = validateEmail;
