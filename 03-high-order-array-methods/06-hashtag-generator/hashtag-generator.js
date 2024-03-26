aa7b60d9981916c543ab0600f4eaa5a0011f8bee
Julia Darwish <darwishjulia4@gmail.com>
2024-03-26 10:09:42 -0400

    Add  generateHashTag func

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,14 +1,20 @@
-function generateHashtag(str) {
-  if (str.trim() === "") {
-    return false;
-  }
-  let hashtag = str
-    .trim()
-    .split(" ")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join("");
+// function generateHashtag(str) {
+//   if (str.trim() === "") {
+//     return false;
+//   }
+//   let hashtag = str
+//     .trim()
+//     .split(" ")
+//     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+//     .join("");
 
-  return hashtag.length > 140 ? false : `#${hashtag}`;
+//   return hashtag.length > 140 ? false : `#${hashtag}`;
+// }
+function generateHashtag(str) {
+  let hashtag = str.split(" ").reduce((acc, word) =>{
+    return acc + word.charAt(0).toUpperCase() + word.slice(1) 
+  }, "#")
+  return hashtag.length === 1 || hashtag.length > 140 ? false : hashtag;
 }
 
 module.exports = generateHashtag;
