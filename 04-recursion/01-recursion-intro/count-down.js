a256f2ca905c17b494111f3faeb47e1662f4b410
Julia Darwish <darwishjulia4@gmail.com>
2024-03-27 21:51:37 -0400

    Update recursion func

---
+++ 04-recursion/01-recursion-intro/count-down.js
@@ -0,0 +1,13 @@
+function countDown(num) {
+  //base case
+  if ((num <= 0)) {
+    console.log("All done!");
+    return;
+  }
+  //   Recursive case
+  console.log(num);
+  num--;
+  countDown(num);
+}
+
+module.exports = countDown;
