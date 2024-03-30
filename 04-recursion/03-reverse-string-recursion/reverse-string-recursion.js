0c46e6631a04240ae7d1b9cc2cee663a650afa0e
Julia Darwish <darwishjulia4@gmail.com>
2024-03-30 08:14:47 -0400

    Update reverseString func

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -5,8 +5,9 @@
 //     return reverseString(str.substr(1)) + str.charAt(0);
 // }
 
-const reverseString=(str)=> str === "" ? "" : reverseString(str.substr(1)) + str.charAt(0);
+// const reverseString=(str)=> str === "" ? "" : reverseString(str.substr(1)) + str.charAt(0);
 
+const reverseString = (str)=> str === "" ? "" : reverseString(str.substr(1)) + str.charAt(0);
 
 
 module.exports = reverseString;
